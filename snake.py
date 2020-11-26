c5818ce40de1e1cbfe4316402c787f4e24355105
keita1988 <keita12may@gmail.com>
2020-11-27 03:37:46 +0900

    Initial commit

---
+++ snake.py
@@ -0,0 +1,42 @@
+width = 500
+height = 500
+rows = 20
+
+class Cube():
+  def __init__(self, start, x=1, y=0, color=(255, 0, 0)):
+    pass
+
+  def move(self, x, y):
+    pass
+
+  def draw(self, surface, eyes=False):
+    pass
+
+
+class Snake():
+  def __init__(self, color, pas):
+    pass
+  
+  def move(self):
+    pass
+
+  def reset(self, pas):
+    pass
+
+  def addBody(self):
+    pass
+
+  def draw(self, surface):
+    pass
+
+def reDrawWindow():
+  pass
+
+def drawGrid(width, rows, surface):
+  pass
+
+def randomFood(rows, food):
+  pass
+
+def main():
+  pass
